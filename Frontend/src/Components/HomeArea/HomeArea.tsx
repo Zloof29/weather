28f0e606627148f95489cf9b1bdf929930f92e37
Lior Bas <look551@gmail.com>
2025-01-26 19:29:46 +0200

    fixed & redux

---
+++ Frontend/src/Components/HomeArea/HomeArea.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react";
+import "./HomeArea.css";
+
+export function HomeArea(): JSX.Element {
+  return (
+    <div className="HomeArea">
+      <p>Home...</p>
+    </div>
+  );
+}
